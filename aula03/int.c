6d09e7c0f0eb8eea8aa934903c5c4bdac2c00abb
CarlosMartins <carlosmartins00098>
2024-01-24 21:26:24 -0300

    int.c de 64 bits

--- aula03/int.c
+++ aula03/int.c
@@ -3,10 +3,10 @@
 #include <stdint.h>
 
 int main(void){
- uint8_t i = CHAR_MAX; 
+  long long i;
 
   printf("O tamanho de i (int): %zu bytes %zu bits ",sizeof i, sizeof i * 8);
-  printf("\nValor de i:%u\n",i);
+  printf("\nValor de i:%lld\n",i);
 
   return 0;
 }
