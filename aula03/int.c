521edaa785365e47888133f5e4750cc5e8626f41
CarlosMartins <carlosmartins00098>
2024-01-24 21:03:50 -0300

    int.c de 8 bits

--- aula03/int.c
+++ aula03/int.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 int main(void){
- uint16_t i = USHRT_MAX; 
+ uint8_t i = CHAR_MAX; 
 
   printf("O tamanho de i (int): %zu bytes %zu bits ",sizeof i, sizeof i * 8);
   printf("\nValor de i:%u\n",i);
