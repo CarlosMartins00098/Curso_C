3f444930c3780e1b44b69824f324798aa1c35a58
CarlosMartins <carlosmartins00098>
2024-01-24 21:38:38 -0300

    int.c register (registrador)

--- aula03/int.c
+++ aula03/int.c
@@ -3,10 +3,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 int main(void){
-  size_t i;
+  register int i;
 
   printf("O tamanho de i (int): %zu bytes %zu bits ",sizeof i, sizeof i * 8);
-  printf("\nValor de i:%zu\n",i);
+  printf("\nValor de i:%d\n",i);
 
   return 0;
 }
