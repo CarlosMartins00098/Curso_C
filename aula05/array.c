df49a50e2a784dcd42c33ba5cae2af7d040dcdd8
CarlosMartins <carlosmartins00098>
2024-02-02 17:09:47 -0300

    tmp

---
+++ aula05/array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(void){
+  int c[3];
+
+  printf("O tamanho de c eh: %zu\n",sizeof c);
+
+  return 0;
+
+}
