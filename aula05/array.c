0ffd7f35ffdbf8789e616e0f573ffd55af7b204e
CarlosMartins <carlosmartins00098>
2024-02-02 18:07:19 -0300

    Código Fonte da aula 5

--- aula05/array.c
+++ aula05/array.c
@@ -4,9 +4,26 @@
 #include <stdlib.h>
 
 int main(void){
-  int c[3];
+  long int c[3];
 
   printf("O tamanho de c eh: %zu\n",sizeof c);
+  printf("O número de elememtos em c eh: %zu\n",sizeof c / sizeof c[0]);
+
+  c[0]='A';
+  c[1]= 0x42;
+  c[2]= 67;
+
+  printf("O tamanho 0 de c eh: %ld\n", c[0]);
+  printf("O tamanho 1 de c eh: %ld\n", c[1]);
+  printf("O tamanho 2 de c eh: %ld\n", c[2]);
+  printf("O tamanho 3 de c eh: %ld\n", c[2]);
+  
+  printf("O endereço de c em memoria: %p\n", c);
+
+  printf("O endereço   &c em memoria: %p\n", &c[0]);
+  printf("O endereço do c em memoria: %p\n", &c[1]);
+
+
 
   return 0;
 
