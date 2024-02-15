e142d75aa432088865d24ee119b9471dda40c235
CarlosMartins <carlosmartins00098>
2024-02-15 14:13:39 -0300

    invertendo os binários

---
+++ aula08/opbit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * &
+ * |
+ * ^ xor
+ * ~ not
+ * <<
+ * >>
+ */
+
+int main(void){
+  int a = 0b1010;   
+  int b = 0b011111111111111111111111111111111;
+
+  // 0 falso
+
+  printf("\nO resultado e: %d\n",  a);
+
+  return 0;
+
+}
