ebd2e9c2c1c9a855df1f15c4b4ca39d03bc4babf
CarlosMartins <carlosmartins00098>
2024-03-07 19:37:10 -0300

    Código Fonte da aula 9

---
+++ aula09/if.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+  int a = 359;
+
+  if(-1 == a || a < 400)
+  {
+    int chave = 3;
+    printf("chave: %d\n", chave);
+  
+
+  }
+
+  printf("\nO resultado e: %d\n", a );
+  puts("hello kitty");
+
+  return 0;
+
+}
